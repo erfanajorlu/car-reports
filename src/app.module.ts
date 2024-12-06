b570c76d0b4a8b00655140b8df58a0b6913a32ac
Erfan Ajorlu <erfanajoglu098@gmail.com>
2024-12-06 23:38:56 +0330

    handle config for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
+import * as ormconfig from '../ormconfig';
 const cookieSession = require('cookie-session');
 @Module({
   imports: [
@@ -15,22 +14,8 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: ,
-    //   entities: [User, Report],
-    //   synchronize: true,
-    // }),
     TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: true,
-          entities: [User , Report]
-        }
-      }
+      useFactory: () => ormconfig,
     }),
     UsersModule,
     ReportsModule,
